f6c22fd04b1a7d40f96bfb44ba0db3d8eef3ff55
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-01-28 15:47:12 +0100

    add JSnack3 complete

--- JSnack3/js/main.js
+++ JSnack3/js/main.js
@@ -1,16 +1,23 @@
 // JSnack3
 // Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
-
-let numbers = [
-    1, 22, 3, 54, 77, 88, 17];
+const totalNumbersEl = document.getElementById("total-numbers");
+const sumEvenNumbers = document.getElementById("sum-even-numbers");
+const evenNumbers = document.getElementById('even-numbers');
+let numbers = [1, 22, 3, 54, 77, 88, 17];
 // console.log(numbers.length);
 
 let sum = 0;
 
 for (let i = 0; i < numbers.length; i++) {
-    // console.log(numbers[i]);
-    if (i % 2 !== 1) {
-        sum += numbers[i];
-    }
+  console.log(numbers[i]);
+  if (i % 2 !== 1) {
+    sum += numbers[i];
+evenNumbers.innerHTML += `${numbers[i]}   `;
+  }
+  totalNumbersEl.innerHTML += `${numbers[i]}  `;
+  console.log(totalNumbersEl)
+  sumEvenNumbers.innerHTML = `${sum} `;
+  // console.log(sumEvenNumbers);
 }
-console.log(sum);
+// console.log(sum);
+// console.log(sumEvenNumbers);
